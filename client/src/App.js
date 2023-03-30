81450f3c80d36fa0dfee17c50d48aadd66dcc8f8
JayNandasana <90906772+JayNandasana@users.noreply.github.com>
2023-03-30 14:40:29 +0530

    user credentials stored in localstorage, add admin

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import Nevbar from "./components/Nevbar";
 import Templates from "./components/Templates";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import GetLogin from "./components/GetLogin";
 import GetRegister from "./components/GetRegister";
 import Login from "./components/Login";
 import DataProvider from "./context/DataProvider";
 import Home from "./components/Home";
+import Admin from "./components/Admin";
+import { useContext } from "react"; 
+import { DataContext } from "./context/DataProvider";
 
 function App() {
   return (
@@ -18,10 +21,20 @@ function App() {
           <Route path="GetLogin" element={<GetLogin/>} />
           <Route path="GetRegister" element={<GetRegister/>} />
           <Route path="Login" element={<Login/>} />
+          <Route path="Admin" element={<ProtectedAdmin/>} />
         </Routes>
       </BrowserRouter>
     </DataProvider>
   );
+}
+
+function ProtectedAdmin() {
+  const { account } = useContext(DataContext);
+  if (account === "admin") {
+    return <Admin />; 
+  } else {
+    // return <Navigate to="/" replace={true} />;
+  }
 }
 
 export default App;
